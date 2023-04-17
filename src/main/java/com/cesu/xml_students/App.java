619c1a2587fcc7ac0af246ec49541bbfde440a00
asuD <jefax@tutamail.com>
2023-04-17 13:36:14 +0200

    inital maven app structure

---
+++ src/main/java/com/cesu/xml_students/App.java
@@ -0,0 +1,13 @@
+package com.cesu.xml_students;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
