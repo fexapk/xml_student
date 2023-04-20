1572c3ef9d7aca6bce2367aef48931a8d22f6ba6
asuP <jefax@tutamail.com>
2023-04-20 14:35:20 +0200

    Started developing AlumnoTransformer

--- src/main/java/com/cesu/xml_students/App.java
+++ src/main/java/com/cesu/xml_students/App.java
@@ -2,12 +2,11 @@ package com.cesu.xml_students;
 
 /**
  * Hello world!
- *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
---
+++ src/main/java/com/cesu/xml_students/data_writing/AlumnosTransformer.java
@@ -0,0 +1,88 @@
+package com.cesu.xml_students.data_writing;
+
+import com.cesu.xml_students.pjo.*;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.util.List;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import java.io.File;
+
+public class AlumnosTransformer {
+
+    private String outFilePath = null;
+    private Document document = null;
+    private List<Alumno> alumList = null;
+
+
+    public AlumnosTransformer(String outFilePath, Document document, List<Alumno> alumList) {
+        if (outFilePath.isBlank() || outFilePath == null)
+            throw new IllegalArgumentException("Must be a valid path");
+        if (document == null || alumList == null)
+            throw new IllegalArgumentException("Either document or list must point somewhere");
+        this.outFilePath = outFilePath;
+        this.document = document;
+        this.alumList = alumList;
+    }
+
+    /**
+     * Basically writes dom content to xml file, returns true if it succeded in writing the file
+     * @return (boolean)
+     */
+    public boolean saveDocToFile() {
+        try {
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes"); // enable indentation
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2"); // set indentation amount
+
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File(outFilePath));
+            transformer.transform(source, result);
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            return false;
+        }
+        return true;
+    }
+    
+    public void updateDoc() {
+        
+    }
+
+    
+
+    /**
+     * Transforms Alumno Obj to it's dom counterpart
+     * @param alumno (Alumno)
+     * @return (Element)
+     */
+    private  Element transformAlumno(Alumno alumno) {
+        Element alumElement = document.createElement("Alumno");
+
+        Element id = document.createElement("id");
+        id.appendChild(document.createTextNode(String.valueOf(alumno.getId())));
+        alumElement.appendChild(id);
+
+        alumElement.appendChild(createStrData("Nombre", alumno.getNombre()));
+        alumElement.appendChild(createStrData("Apellido", alumno.getApellido()));
+        alumElement.appendChild(createStrData("Grado", alumno.getGrado()));
+        alumElement.appendChild(createStrData("FechaFin", alumno.getFechaFin()));
+
+        return alumElement;
+    }
+
+    private Element createStrData(String tag, String data) {
+        Element id = document.createElement("id");
+        id.appendChild(document.createTextNode(data));
+        return id;
+    }
+
+}
